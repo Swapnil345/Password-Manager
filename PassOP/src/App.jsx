ed1987927f53d52d357e7b3f72df818abf939a4d
Swapnil345 <swapnilkm03@gmail.com>
2025-10-03 15:23:01 +0530

    Password manager website setup and navbar creation

---
+++ PassOP/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import Manager from './components/Manager'
+import Navbar from './components/Navbar'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Manager/>
+    </>
+  )
+}
+
+export default App
