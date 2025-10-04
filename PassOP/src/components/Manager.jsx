a15d3d622de359b01b6e7fe5a0583e8a729cde13
Swapnil345 <swapnilkm03@gmail.com>
2025-10-04 12:53:32 +0530

    button modification

--- PassOP/src/components/Manager.jsx
+++ PassOP/src/components/Manager.jsx
@@ -3,23 +3,27 @@ import React from 'react'
 const Manager = () => {
     return (
     <>
-        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
-        <div className="bg-slate-50 mycontainer">
+        <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+        <div className="mycontainer">
             <h1 className='text-4xl text font-bold text-center'>
-                <span className="text-green-700"> &lt;</span>
+                <span className="text-green-500"> &lt;</span>
                 <span>Pass</span>
-                <span className="text-green-700">OP/&gt;</span>
+                <span className="text-green-500">OP/&gt;</span>
             </h1>
             <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
-            <div className="flex flex-col p-4 text-black gap-8">
+            <div className="flex flex-col p-4 text-black gap-8 items-center">
             <input className='rounded-full border border-green-500 w-full px-4 py1' type="text" name="" id="" />
             <div className="flex w-full justify-between gap-8">
                 <input className='rounded-full border border-green-500 w-full px-4 py1' type="text" name="" id="" />
                 <input className='rounded-full border border-green-500 w-full px-4 py1' type="text" name="" id="" />
 
-                
                 </div>
-                <button>Add Password</button>
+                <button className='flex justify-center items-center gap-2 bg-green-600 hover:bg-green-500 rounded-full px-8 py-2 w-fit cursor-pointer'>
+                <lord-icon
+                    src="https://cdn.lordicon.com/efxgwrkc.json"
+                    trigger="hover">
+                </lord-icon>
+                Add Password</button>
         </div>
         </div>
 
